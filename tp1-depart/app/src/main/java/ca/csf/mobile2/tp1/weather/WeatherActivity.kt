c9b3d0ddf86d0b12dc2860399782f1d1e5c0222e
Jacob Ramsay <ramsayjacob@gmail.com>
2019-01-15 13:16:12 -0500

    Initial commit

---
+++ tp1-depart/app/src/main/java/ca/csf/mobile2/tp1/weather/WeatherActivity.kt
@@ -0,0 +1,14 @@
+package ca.csf.mobile2.tp1.weather
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ca.csf.mobile2.tp1.R
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+
+}
